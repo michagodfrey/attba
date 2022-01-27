894316ae04d9159536de28320da6b6332246b536
Michael Godfrey <m.godfrey59@gmail.com>
2022-01-27 22:08:02 +1100

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <main>
+        <section className='centered'>
+            <h2>under construction</h2>
+        </section>
+    </main>
+  ); 
+};
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home"
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about"
+  },
+  {
+    id: 3,
+    url: "/biosecurity",
+    text: "biosecurity"
+  },
+  {
+    id: 4,
+    url: "/animals",
+    text: "animals"
+  },
+  {
+    id: 5,
+    url: "/plants",
+    text: "plants"
+  },
+  {
+    id: 6,
+    url: "/producers & traders",
+    text: "producers & traders"
+  },
+  {
+    id: 7,
+    url: "/travelers",
+    text: "travelers"
+  }
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.facebook.com",
+    icon: <FaFacebook />
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />
+  },
+  {
+    id: 3,
+    url: "https://www.linkedin.com",
+    icon: <FaLinkedin />
+  },
+];
+
